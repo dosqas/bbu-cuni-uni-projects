2d25dc25c2652e423ffdc68336f61bde03d419b3
dosqas <137123562+dosqas@users.noreply.github.com>
2025-04-29 05:23:48 +0300

    Add CORS headers to fetch_product.php for API accessibility

--- 2ndYr_Sem2/WEB-PROG/LAB7/ajax/fetch_product.php
+++ 2ndYr_Sem2/WEB-PROG/LAB7/ajax/fetch_product.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 require '../includes/db.php'; // Include the database connection
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
