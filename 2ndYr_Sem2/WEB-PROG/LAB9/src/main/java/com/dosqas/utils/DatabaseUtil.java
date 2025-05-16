c733535cebbe0279b9c56870a2f70b920208b119
dosqas <137123562+dosqas@users.noreply.github.com>
2025-05-16 15:33:08 +0300

    feat: add DatabaseUtil for managing database connections

---
+++ 2ndYr_Sem2/WEB-PROG/LAB9/src/main/java/com/dosqas/utils/DatabaseUtil.java
@@ -0,0 +1,21 @@
+package com.dosqas.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/puzzle_app";
+    private static final String USER = "root";
+    private static final String PASSWORD = "admin";
+
+    public static Connection getDatabaseConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver not found", e);
+        }
+
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
