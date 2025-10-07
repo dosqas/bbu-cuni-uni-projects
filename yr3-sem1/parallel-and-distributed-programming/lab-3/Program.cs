46d67452876217d7705fc1b00f6d92b1d78cb043
dosqas <137123562+dosqas@users.noreply.github.com>
2025-10-07 13:40:03 +0300

    wip version of pdp lab 3

--- yr3-sem1/parallel-and-distributed-programming/lab-3/Program.cs
+++ yr3-sem1/parallel-and-distributed-programming/lab-3/Program.cs
@@ -1,2 +1,146 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+namespace lab_3;
+
+class Program
+{
+    static readonly int matrixSize = 9;
+    static readonly int functionVersion = 1; 
+
+    static int[,] matrixA = new int[matrixSize, matrixSize];
+    static int[,] matrixB = new int[matrixSize, matrixSize];
+    static int[,] resultMatrix = new int[matrixSize, matrixSize];
+
+    static int Compute_element(int row, int col, int thread_number)
+    {
+        Console.WriteLine($"Thread {thread_number} computing element [{row}, {col}]");
+
+        int sum = 0;
+        for (int k = 0; k < matrixSize; k++)
+        {
+            sum += matrixA[row, k] * matrixB[k, col];
+        }
+        return sum;
+    }
+
+    static void Compute_product_row_by_row(int startIdx, int endIdx, int thread_number)
+    {
+        for (int k = startIdx; k < endIdx; k++)
+        {
+            resultMatrix[k / matrixSize, k % matrixSize] = Compute_element(k / matrixSize, k % matrixSize, thread_number);
+        }
+    }
+
+    static void Compute_product_col_by_col(int startIdx, int endIdx, int thread_number)
+    {
+        for (int k = startIdx; k < endIdx; k++)
+        {
+            resultMatrix[k % matrixSize, k / matrixSize] = Compute_element(k % matrixSize, k / matrixSize, thread_number);
+        }
+    }
+
+    static void Compute_product_every_k(int startIdx, int k, int amount, int thread_number)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            int index = startIdx + i * k;
+            resultMatrix[index / matrixSize, index % matrixSize] = Compute_element(index / matrixSize, index % matrixSize, thread_number);
+        }
+    }
+
+    static void Generate_matrices()
+    {
+        matrixA = new int[matrixSize, matrixSize];
+        matrixB = new int[matrixSize, matrixSize];
+
+        var rand = new Random();
+        for (int i = 0; i < matrixSize; i++)
+        {
+            for (int j = 0; j < matrixSize; j++)
+            {
+                matrixA[i, j] = rand.Next(1, 4);
+                matrixB[i, j] = rand.Next(1, 4);
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        for (int thread_count = 4; thread_count <= 10; thread_count++)
+        {
+            Console.WriteLine("===============================================");
+            Console.WriteLine($"Starting experiment with {thread_count} threads");
+            for (int matrix_size = 10; matrix_size <= 11; matrix_size++)
+            {
+                Console.WriteLine($"\n[{matrix_size - 6}] Matrix size: {matrix_size}x{matrix_size}");
+                Generate_matrices();
+
+                var tasks = new List<Task>();
+
+                var stopwatch = Stopwatch.StartNew();
+
+                for (int k = 0; k < thread_count; k++)
+                {
+                    int threadNum = k;
+                    int startIdx = threadNum * (matrixSize * matrixSize) / thread_count;
+                    int endIdx = (threadNum + 1) * (matrixSize * matrixSize) / thread_count;
+                    var t = new Task(() =>
+                    {
+                        switch (functionVersion)
+                        {
+                            case 1:
+                                Compute_product_row_by_row(startIdx, endIdx, threadNum);
+                                break;
+                            case 2:
+                                Compute_product_col_by_col(startIdx, endIdx, threadNum);
+                                break;
+                            case 3:
+                                Compute_product_every_k(startIdx, thread_count, (matrixSize * matrixSize) / thread_count, threadNum);
+                                break;
+                        }
+                    });
+
+                    tasks.Add(t);
+                }
+
+                tasks.ForEach(t => t.Start());
+
+                Task.WaitAll([.. tasks]);
+
+                stopwatch.Stop();
+                Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds} ms");
+
+                Console.WriteLine("\nMatrix A:");
+                for (int i = 0; i < matrixSize; i++)
+                {
+                    for (int j = 0; j < matrixSize; j++)
+                    {
+                        Console.Write(matrixA[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("Matrix B:");
+                for (int i = 0; i < matrixSize; i++)
+                {
+                    for (int j = 0; j < matrixSize; j++)
+                    {
+                        Console.Write(matrixB[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("Result Matrix:");
+                for (int i = 0; i < matrixSize; i++)
+                {
+                    for (int j = 0; j < matrixSize; j++)
+                    {
+                        Console.Write(resultMatrix[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
